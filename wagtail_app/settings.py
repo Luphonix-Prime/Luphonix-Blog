53b8e7132e31e547504631810ab3d36f03199391
Dhyey Khanpara <106962485+dhyeykhanpara21@users.noreply.github.com>
2025-05-09 11:44:16 +0530

    refactor: remove redundant CSS link and fix settings
    - Removed redundant conditional CSS link in profile_completion.html to simplify the template
    - Fixed indentation in middleware list and updated STATIC_ROOT path in wagtail_app/settings.py
    - Deleted duplicate settings.py file to avoid configuration conflicts

--- wagtail_app/settings.py
+++ wagtail_app/settings.py
@@ -82,7 +82,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'wagtail_app.middleware.ProfileCompletionMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
     
@@ -156,7 +156,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'collectstatic')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'wagtail_app', 'static'),
